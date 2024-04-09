e03b4ebf5812c1c60f4a0039941ebef112ed9919
Gabriel Araújo Souza <107129598+GSOUZA11@users.noreply.github.com>
2024-04-09 19:33:03 -0300

    Update Questão1.js

--- Questão1.js
+++ Questão1.js
@@ -8,4 +8,4 @@ for (let i = 1; i <= 10; i++) {
 // Objetivo e mostrar os numeros de 1 a 10 no console um por vez.
 //Usei o Node para executar esse codigo no terminal. 
 //let i= 1 Defini o valor inicial da variável de controle da repetição.
-//i <= 20; Testa variável de  controle para saber  se haverá  proxima iteração do loop, enquanto for menor ou igual a 20 continuará
+//i <= 20; Testa variável de  controle para saber  se haverá  proxima iteração do loop, enquanto for menor ou igual a 10 continuará
